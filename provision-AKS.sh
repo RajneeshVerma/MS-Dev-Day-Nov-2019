f6ff7c6096f920507ce46ebfae4a5a54924ee162
James Parks <James.Parks@agilethought.com>
2019-11-12 19:39:36 -0500

    change aks provisioning from python to cmd

---
+++ provision-AKS.sh
@@ -0,0 +1,13 @@
+az login
+az group create --name <a_resource_group_name> --location <a_region_name>
+az aks create --resource-group <the_resource_group_name> \
+    --name <a_cluster_name> \
+    --node-count <an_integer> \
+    --vm-set-type VirtualMachineScaleSets \
+    --load-balancer-sku standard \
+    --enable-cluster-autoscaler \
+    --generate-ssh-keys \
+    --node-vm-size <a_vm_sku> \
+    --min-count <a_small_integer> \
+    --max-count <a_larger_integer> \
+az aks update --resource-group <the_resource_group_name> --name <the_cluster_name> --disable-cluster-autoscaler
