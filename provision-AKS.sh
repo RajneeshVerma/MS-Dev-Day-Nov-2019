dd9e76083254a60d9798df7ec303c195cd5a358a
Mark Schabacker <mark.schabacker@gmail.com>
2019-11-13 20:16:17 +0000

    Hard code AKS provision values

--- provision-AKS.sh
+++ provision-AKS.sh
@@ -1,13 +1,17 @@
 az login # Not required in Azure Cloud Shell
-az group create --name <a_resource_group_name> --location <a_region_name>
-az aks create --resource-group <the_resource_group_name> \
-    --name <a_cluster_name> \
-    --node-count <an_integer> \
+az group create --name atDevDayWorkshopRG --location eastus
+
+az provider register --namespace Microsoft.Network
+az provider register --namespace Microsoft.Compute
+az provider register --namespace Microsoft.Storage
+
+az aks create --resource-group atDevDayWorkshopRG \
+    --name atDevDayCluster \
+    --node-count 1 \
     --vm-set-type VirtualMachineScaleSets \
-    --load-balancer-sku standard \
     --enable-cluster-autoscaler \
     --generate-ssh-keys \
-    --node-vm-size <a_vm_sku> \
-    --min-count <a_small_integer> \
-    --max-count <a_larger_integer> \
-az aks update --resource-group <the_resource_group_name> --name <the_cluster_name> --disable-cluster-autoscaler
+    --node-vm-size Standard_D2_v3 \
+    --min-count 1 \
+    --max-count 2
+az aks update --resource-group atDevDayWorkshopRG --name atDevDayCluster --disable-cluster-autoscaler
