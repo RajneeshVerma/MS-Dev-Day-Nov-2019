59cab096442133e1770bc0156837bc869a85ada9
James Parks <James.Parks@agilethought.com>
2019-11-13 21:32:53 -0500

    new repo structure

---
+++ release_pipeline_scripts/run-AKSController.sh
@@ -0,0 +1,6 @@
+docker login msftdevdayamlcr.azurecr.io/ --username $ACR_NAME --password $ACR_PASS
+docker run -v $(System.DefaultWorkingDirectory)/_model-build/mlops-pipelines/python_scripts/:/script \
+ -w=/script -e SP_APP_ID=$SP_APP_ID -e SP_APP_SECRET=$SP_APP_SECRET -e TENANT_ID=$TENANT_ID \
+ -e AKS_RG=$AKS_RG -e STORAGE_ACCT_NAME=$STORAGE_ACCT_NAME -e STORAGE_ACCT_KEY=$STORAGE_ACCT_KEY \
+ -e STORAGE_BLOB_NAME=$STORAGE_BLOB_NAME -e CONTANER_NAME=$CONTANER_NAME -e AKS_NAME=$AKS_NAME \
+msftdevdayamlcr.azurecr.io/aks:latest python AksResourceController.py
--- release_pipeline_scripts/train-models.sh
+++ release_pipeline_scripts/train-models.sh
@@ -1,4 +1,4 @@
-docker run -v $(System.DefaultWorkingDirectory)/_model-build/mlops-pipelines/:/script \
+docker run -v $(System.DefaultWorkingDirectory)/_model-build/mlops-pipelines/python_scripts/:/script \
  -w=/script -e MODEL_NAME=$MODEL_NAME -e EXPERIMENT_NAME=$EXPERIMENT_NAME \
  -e TENANT_ID=$TENANT_ID -e SP_APP_ID=$SP_APP_ID -e SP_APP_SECRET=$SP_APP_SECRET \
  -e SUBSCRIPTION_ID=$SUBSCRIPTION_ID -e RELEASE_RELEASEID=$RELEASE_RELEASEID \
